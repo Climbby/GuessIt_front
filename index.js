802196df399c90acbaaba8357febe9861a1634b6
Climbby <franciscoaf.gaming@gmail.com>
2025-03-11 15:36:08 +0000

    made index.js cleaner

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
 fetch("https://guess-it-neon.vercel.app/api/users")
   .then((res) => res.json())
   .then((data) => {
-    console.log("Users from database:", data);
-    // Display them in HTML (example):
+
     const list = document.getElementById("users");
 
-    if (Array.isArray(data)) {
-        data.forEach((user) => {
-          console.log("user.id:", user.id);
-          console.log("user.name:", user.name);
-        });
-    } else {
-      console.error("Received data is not an array:", data);
-    }
-    
     data.forEach((user) => {
-      console.log("user.id:", user.id);
-      console.log("user.name:", user.name);
       const li = document.createElement("li");
       li.textContent = `ID: ${user.id} | Username: ${user.name}`;
       list.appendChild(li);
