eb590da89df652f6027bc920eadf96065ac6ea62
Climbby <franciscoaf.gaming@gmail.com>
2025-03-12 22:24:33 +0000

    Helped with html structure

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ fetch("https://guess-it-neon.vercel.app/api/users")
   .then((res) => res.json())
   .then((data) => {
 
-    // const list = document.getElementById("users");
+    const list = document.getElementById("users");
 
     data.forEach((user) => {
-      // const li = document.createElement("li");
-      // li.textContent = `NOME: ${user.nome} | Username: ${user.grupo}`;
-      // list.appendChild(li);
+      const li = document.createElement("li");
+      li.textContent = `${user.nome}`;
+      list.appendChild(li);
 
-      console.log(`${user.nome}: ${user.idade} anos, beleza ${user.beleza}, ${user.altura}cm, vibe: ${user.vibe}, ${user.corpo}, ${user.socializacao}, ${user.ocupacao}, ${user.grupo}`);
+      // console.log(`${user.nome}: ${user.idade} anos, beleza ${user.beleza}, ${user.altura}cm, vibe: ${user.vibe}, ${user.corpo}, ${user.socializacao}, ${user.ocupacao}, ${user.grupo}`);
 
     });
   })
