2e8030051938bc05e2eec17bdbfb5646d91e8941
Climbby <franciscoaf.gaming@gmail.com>
2025-03-12 09:48:20 +0000

    logging characteristics after db update

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ fetch("https://guess-it-neon.vercel.app/api/users")
   .then((res) => res.json())
   .then((data) => {
 
-    const list = document.getElementById("users");
+    // const list = document.getElementById("users");
 
     data.forEach((user) => {
-      const li = document.createElement("li");
-      li.textContent = `NOME: ${user.nome} | Username: ${user.grupo}`;
-      list.appendChild(li);
+      // const li = document.createElement("li");
+      // li.textContent = `NOME: ${user.nome} | Username: ${user.grupo}`;
+      // list.appendChild(li);
+
+      console.log(` ${user.nome}: ${user.idade} anos, beleza ${user.beleza}, ${user.altura}cm, vibe: ${user.vibe}, 
+                    ${user.corpo}, ${user.socializacao}, ${user.ocupacao}, ${user.grupo}`);
+
     });
   })
   .catch((err) => console.error("Error fetching users:", err));
