e48bd73aad22c87887b80d88f2f30e6a7b69b916
Climbby <franciscoaf.gaming@gmail.com>
2025-03-15 17:00:08 +0000

    Cleaner code, added ReadMe to explain process

--- js/main.js
+++ js/main.js
@@ -1,32 +1,14 @@
+import { initAutoComplete, submitAnswer } from './utils/events.js';
 import { fetchUsers } from './api.js';
-import { initAutocomplete, submitAnswer } from './utils/events.js';
-
-let users = [];
-let randomUser = null;
-let numCharacteristicas = 0;
 
 fetchUsers().then(data => {
-  data.forEach(user => {
-    const caracteristicas = {
-      nome: user.nome,
-      beleza: user.beleza,
-      altura: user.altura,
-      vibe: user.vibe,
-      corpo: user.corpo,
-      socializacao: user.socializacao,
-      ocupacao: user.ocupacao,
-      grupo: user.grupo,
-    };
-
-    users.push(caracteristicas);
-  });
-
-  numCharacteristicas = Object.values(users[0]).length;
+  
+  const users = data
   const randomIndex = Math.floor(Math.random() * users.length);
-  randomUser = users[randomIndex];
+  const randomUser = users[randomIndex];
 
   //events
-  initAutocomplete(users);
-  submitAnswer(users, randomUser, numCharacteristicas);
+  initAutoComplete(users, randomUser);
+  submitAnswer(users, randomUser);
 
 });
--- js/utils/events.js
+++ js/utils/events.js
@@ -1,106 +1,117 @@
 import { button, answersContainer, inputText } from '../domElements.js'
 import { autoShrinkText, arraysEqual } from './funcs.js';
 
-export function initAutocomplete(users) {  
-    const userNames = users.map(user => user.nome);
-    const searchBox = document.querySelector('#myInput');
-    const dropdown = document.querySelector('#dropdown');
-    let filtered;
-  
-    searchBox.addEventListener("input", () => {
-      const input = searchBox.value.toLowerCase();
-      dropdown.innerHTML = "";
-      if (input === "") {
-        dropdown.style.display = "none";
-        return;
-      }
-  
-      filtered = userNames.filter(option => option.toLowerCase().includes(input));
+export function initAutoComplete(users, randomUser) {  
 
-      if (filtered.length > 0) {
-        filtered.forEach(option => {
-          const item = document.createElement("div");
-          item.textContent = option;
-          item.onclick = () => {
-            searchBox.value = option;
-            dropdown.style.display = "none";
-          };
-          dropdown.appendChild(item);
-        });
-        dropdown.style.display = "block";
-      } else {
-        dropdown.style.display = "none";
-      }
-    });
-  
-    document.addEventListener("click", (e) => {
-      if (e.target !== searchBox) {
-        dropdown.style.display = "none";
-      }
-      if (e.target == searchBox && searchBox.value !== "") {
-        dropdown.style.display = "block";
-      }
-    });
+  const dropdown = document.querySelector('#dropdown');
+
+  inputText.addEventListener("input", () => {
+    const input = inputText.value.toLowerCase();
+    const userNames = users.map(user => user.nome);
+    const filtered = userNames.filter(option => option.toLowerCase().includes(input));
+    dropdown.innerHTML = "";
+
+    if (input === "") {
+      dropdown.style.display = "none";
+      return;
+    }
+
+    if (filtered.length > 0) {
+      filtered.forEach(option => {
+        const item = document.createElement("div");
+        item.textContent = option;
+        item.onclick = () => {
+          inputText.value = option;
+          handleButtonClick(users, randomUser);  
+          dropdown.style.display = "none";
+        };
+        dropdown.appendChild(item);
+      });
+      dropdown.style.display = "block";
+    } 
+    else {
+      dropdown.style.display = "none";
+    }
+  });
+
+  document.addEventListener("click", e => {
+    if (e.target !== inputText) {
+      dropdown.style.display = "none";
+    }
+    if (e.target == inputText && inputText.value !== "") {
+      dropdown.style.display = "block";
+    }
+  });
 }
 
-export function submitAnswer(users, randomUser, numCharacteristicas){
+export function submitAnswer(users, randomUser){
 
-    button.addEventListener('click', () => {
-      users.forEach(person => {
-        if (person.nome.toUpperCase() === inputText.value.toUpperCase()) { // if name is valid
-          const newAnswer = document.createElement('div');
-          newAnswer.classList.add('newAnswer');
-  
-          for (let i = 0; i < numCharacteristicas; i++) {
-            const personVal = Object.values(person)[i];
-            const randomVal = Object.values(randomUser)[i];
-            const newCharacteristic = document.createElement('div');
-            newCharacteristic.classList.add('newCharacteristic');
-            newCharacteristic.textContent = personVal;
-  
-            // Special logic for vibe
-            if (personVal === person.vibe) {
-              newCharacteristic.textContent = personVal.join(', ');
-              personVal.forEach(caracteri => {
-                if (randomVal.includes(caracteri)) {
-                  newCharacteristic.classList.add('contem');
-                }
-              });
-  
-              if (arraysEqual(personVal, randomVal)) {
-                newCharacteristic.classList.remove('contem');
-                newCharacteristic.classList.add('certo');
-              }
+    button.addEventListener('click', () => {handleButtonClick(users, randomUser)});
+}
+
+function handleButtonClick(users, randomUser){
+
+  const numCharacteristicas = Object.values(users[0]).length;
+
+  users.forEach(person => {
+    if (person.nome.toUpperCase() === inputText.value.toUpperCase()) { // if name is valid
+      const newAnswer = document.createElement('div');
+      newAnswer.classList.add('newAnswer');
+
+      for (let i = 0; i < numCharacteristicas; i++) {
+        const personVal = Object.values(person)[i];
+        const randomVal = Object.values(randomUser)[i];
+        const newCharacteristic = document.createElement('div');
+        newCharacteristic.classList.add('newCharacteristic');
+        newCharacteristic.textContent = personVal;
+
+        if (personVal === person.id) continue;
+
+        // Special logic for vibe
+        if (personVal === person.vibe) {
+          newCharacteristic.textContent = personVal.join(', ');
+          personVal.forEach(caracteri => {
+            if (randomVal.includes(caracteri)) {
+              newCharacteristic.classList.add('contem');
             }
-  
-            // Altura logic
-            if (personVal === person.altura) {
-              if (personVal > randomVal) {
-                newCharacteristic.textContent = personVal + ' ↓';
-              } else if (personVal < randomVal) {
-                newCharacteristic.textContent = personVal + ' ↑';
-              }
-            }
-  
-            if (personVal === randomVal) {
-              newCharacteristic.classList.add('certo');
-            }
-  
-            newAnswer.appendChild(newCharacteristic);
+          });
+
+          if (arraysEqual(personVal, randomVal)) {
+            newCharacteristic.classList.remove('contem');
+            newCharacteristic.classList.add('certo');
           }
-  
-          answersContainer.appendChild(newAnswer);
-          autoShrinkText('.newAnswer', '.newCharacteristic');
         }
-      });
-  
-      if (inputText.value.toUpperCase() === randomUser.nome.toUpperCase()) {
-        const winner = document.createElement('p');
-        winner.classList.add('ganhador');
-        winner.textContent = `GANHASTE!! Era o ${randomUser.nome}`;
-        answersContainer.appendChild(winner);
+
+        // Altura logic
+        if (personVal === person.altura) {
+          if (personVal > randomVal) {
+            newCharacteristic.textContent = personVal + ' ↓';
+          } else if (personVal < randomVal) {
+            newCharacteristic.textContent = personVal + ' ↑';
+          }
+        }
+
+        if (personVal === randomVal) {
+          newCharacteristic.classList.add('certo');
+        }
+
+        newAnswer.appendChild(newCharacteristic);
       }
-  
-      inputText.value = "";
-    });
+
+      const index = users.indexOf(person);
+      users.splice(index, 1);
+
+      answersContainer.appendChild(newAnswer);
+      autoShrinkText('.newAnswer', '.newCharacteristic');
+    }
+  });
+
+  if (inputText.value.toUpperCase() === randomUser.nome.toUpperCase()) {
+    const winner = document.createElement('p');
+    winner.classList.add('ganhador');
+    winner.textContent = `GANHASTE!! Era o ${randomUser.nome}`;
+    answersContainer.appendChild(winner);
+  }
+
+  inputText.value = ""; 
 }
