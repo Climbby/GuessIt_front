431be78a75647c1cacecea50f528783f39412acf
Climbby <franciscoaf.gaming@gmail.com>
2025-03-28 20:41:37 +0000

    testee

--- Game/js/api.js
+++ Game/js/api.js
@@ -1,11 +1,15 @@
 const table = "UGC";
-const ApiURL = `https://guess-it-neon.vercel.app/api/tableApi?table=${table}`;
-// const ApiURL = "http://localhost:3000/api/users";
+const ApiURL = "https://guess-it-neon.vercel.app/api/tableApi";
+// const ApiURL = "http://localhost:3000/api/tableApi";
 
 export async function fetchUsers() {
-
     try {
-      const res = await fetch(ApiURL);
+      const res = await fetch(ApiURL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ table })
+      });
+
       return await res.json();
     } catch (err) {
       console.error("Error fetching users:", err);
