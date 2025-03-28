164d0592118034b91385a7c3618803fc6a78e6b7
Climbby <franciscoaf.gaming@gmail.com>
2025-03-28 16:22:50 +0000

    test

--- Game/js/api.js
+++ Game/js/api.js
@@ -3,6 +3,9 @@ const ApiURL = `https://guess-it-neon.vercel.app/api/UGC?table=${table}`;
 // const ApiURL = "http://localhost:3000/api/users";
 
 export async function fetchUsers() {
+
+    console.log(ApiURL);
+
     try {
       const res = await fetch(ApiURL);
       return await res.json();
