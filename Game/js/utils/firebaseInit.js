7fe4b79cd7aafdd0e89a4c9ccb4b73f5cc2f3e90
Climbby <franciscoaf.gaming@gmail.com>
2025-03-29 01:29:17 +0000

    added firebase

---
+++ Game/js/utils/firebaseInit.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBDE9thK-vBgdScEy426yhulsc7YW_j1jI",
+  authDomain: "guessit-58c20.firebaseapp.com",
+  projectId: "guessit-58c20",
+  storageBucket: "guessit-58c20.firebasestorage.app",
+  messagingSenderId: "514634649046",
+  appId: "1:514634649046:web:03af8fca8e3f1a062fe877"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
