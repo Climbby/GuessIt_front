a2fc3a06552f4dc2d225def24d924a508b89d147
Climbby <franciscoaf.gaming@gmail.com>
2025-03-30 09:52:55 +0000

    Removed main page, issues with cookies because of page reload and different domains made the main page useless for security

---
+++ js/gameLogic.js
@@ -0,0 +1,92 @@
+import { answersContainer, inputText, answersLabel, inputContainer, winnerContainer, winnerText, colorInfo } from './utils/domElements.js'
+import { autoShrinkText, arraysEqual } from './utils/utils.js';
+import { isColorHidden } from './utils/globals.js'
+
+export function gameLogic(users, randomUser){
+
+    const numCharacteristicas = Object.values(users[0]).length;
+  
+    users.forEach(person => {
+      if (person.nome.toUpperCase() === inputText.value.toUpperCase()) { // if name is valid
+        const newAnswer = document.createElement('div');
+        newAnswer.classList.add('newAnswer');
+  
+        for (let i = 0; i < numCharacteristicas; i++) {
+          const personVal = Object.values(person)[i];
+          const randomVal = Object.values(randomUser)[i];
+          const newCharacteristic = document.createElement('div');
+          newCharacteristic.classList.add('newCharacteristic');
+          newCharacteristic.textContent = personVal;
+  
+          if (personVal === person.id) continue;
+          if (personVal === person.vibe) caseVibe(newCharacteristic, personVal, randomVal);
+          if (personVal === person.altura) caseAltura(newCharacteristic, personVal, randomVal);
+          if (personVal === randomVal) newCharacteristic.classList.add('certo');
+  
+          newAnswer.appendChild(newCharacteristic);
+        }
+  
+        // Update users array by removing the last user analyzed
+        const index = users.indexOf(person);
+        users.splice(index, 1);
+  
+        // Add row of characteristics
+
+        //  ekjjafhiewhfuihaeuifh
+        // ifahishefaeshlf
+        // asefiehsifhiuashef
+        // aefhsaihfisahef
+        // asefihasifisea  
+        answersContainer.appendChild(newAnswer);
+        animateAnswer(newAnswer);
+        setTimeout(() => autoShrinkText('.newAnswer', '.newCharacteristic'), 800);
+      }
+    });
+  
+    isCorrect(inputText, randomUser);
+
+    // Reset inputText box text
+    inputText.value = ""; 
+    answersLabel.style.display = "flex";
+    if (!isColorHidden()) colorInfo.style.display = "flex";
+}
+
+function caseVibe(newCharacteristic, personVal, randomVal){
+    newCharacteristic.textContent = personVal.join(', ');
+    personVal.forEach(caracteri => {
+        if (randomVal.includes(caracteri)) {
+        newCharacteristic.classList.add('contem');
+        }
+    });
+
+    if (arraysEqual(personVal, randomVal)) {
+        newCharacteristic.classList.remove('contem');
+        newCharacteristic.classList.add('certo');
+    }
+}
+
+function caseAltura(newCharacteristic, personVal, randomVal){
+    if (personVal > randomVal) {
+        newCharacteristic.textContent = personVal + ' ↓';
+    } else if (personVal < randomVal) {
+        newCharacteristic.textContent = personVal + ' ↑';
+    }
+}
+
+function isCorrect(inputText, randomUser){
+    if (inputText.value.toUpperCase() === randomUser.nome.toUpperCase()) {
+        winnerContainer.style.display = "block";
+        winnerText.textContent = `YOU WIN\nAnswer: ${randomUser.nome}`;
+        inputContainer.style.display = "none";
+    }
+}
+
+function animateAnswer(newAnswer) {
+    const answerCharacteristics = newAnswer.querySelectorAll(".newCharacteristic");
+    answerCharacteristics.forEach((box, index) => {
+      setTimeout(() => {
+        box.style.visibility = "visible"; 
+        box.classList.add("pop");
+      }, index * 150); // Delay each letter's animation
+    });
+}
---
+++ js/utils/funcs.js
@@ -0,0 +1,40 @@
+import { answersLabel } from './domElements.js';
+import { autoShrinkText } from './utils.js';
+import { getDropdownItems } from './globals.js';
+
+export function handleSelection(selectedIndex) {
+
+  let dropdownItems = getDropdownItems();
+
+  dropdownItems.forEach((item, index) => {
+    item.classList.remove('selected'); // Remove old selection
+    if (index === selectedIndex) {
+      item.classList.add('selected'); // Add class to selected item
+      item.scrollIntoView({
+        block: 'nearest', // keeps it minimal (no big jump)
+        behavior: 'smooth' // optional — smooth animation
+      });
+    }
+  });
+}
+
+export function makeLabels(person){
+
+  for (let characteristca in person) {
+    if (characteristca == 'id') continue;
+
+    const newLabel = document.createElement('div');
+    newLabel.classList.add('newLabel');
+    newLabel.textContent = characteristca;
+
+    answersLabel.appendChild(newLabel);
+  }
+
+  setTimeout(() => autoShrinkText('#answers-label', '.newLabel'), 200);
+}
+
+export function setTitle(tableName){
+
+  const title = document.getElementById("title");
+  title.textContent = `GuessIt (${tableName})`;
+}
